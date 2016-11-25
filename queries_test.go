69b962f0b1b4a8189189cedc1890ba1e73bb144c
Gilles Fabio <gilles@gillesfabio.com>
2016-11-25 18:44:03 +0100

    Add tests for SoftdDelete, Delete and Save.

--- queries_test.go
+++ queries_test.go
@@ -1,6 +1,7 @@
 package sqlxx
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -40,4 +41,107 @@ func TestFindByParams(t *testing.T) {
 	is.True(user.IsActive)
 	is.NotZero(user.CreatedAt)
 	is.NotZero(user.UpdatedAt)
+}
+
+func TestSave(t *testing.T) {
+	is := assert.New(t)
+
+	db, _, shutdown := dbConnection(t)
+	defer shutdown()
+
+	user := User{
+		Username: "thoas",
+	}
+
+	require.NoError(t, Save(db, &user))
+
+	is.NotZero(user.ID)
+	is.Equal(true, user.IsActive)
+	is.NotZero(user.UpdatedAt)
+
+	user.Username = "gilles"
+	require.NoError(t, Save(db, &user))
+
+	m := map[string]interface{}{"username": "gilles"}
+
+	query := `
+	SELECT count(*)
+	FROM %s
+	WHERE username = :username
+	`
+
+	stmt, err := db.PrepareNamed(fmt.Sprintf(query, user.TableName()))
+	require.NoError(t, err)
+
+	var count int
+
+	err = stmt.Get(&count, m)
+	require.NoError(t, err)
+
+	is.Equal(1, count)
+}
+
+func TestDelete(t *testing.T) {
+	is := assert.New(t)
+
+	db, _, shutdown := dbConnection(t)
+	defer shutdown()
+
+	user := User{
+		Username: "thoas",
+	}
+
+	require.NoError(t, Save(db, &user))
+	require.NoError(t, Delete(db, &user))
+
+	m := map[string]interface{}{"username": "thoas"}
+
+	query := `
+	SELECT count(*)
+	FROM %s
+	WHERE username = :username
+	`
+
+	stmt, err := db.PrepareNamed(fmt.Sprintf(query, user.TableName()))
+	require.NoError(t, err)
+
+	var count int
+
+	err = stmt.Get(&count, m)
+	require.NoError(t, err)
+
+	is.Equal(0, count)
+}
+
+func TestSoftDelete(t *testing.T) {
+	is := assert.New(t)
+
+	db, _, shutdown := dbConnection(t)
+	defer shutdown()
+
+	user := User{
+		Username: "thoas",
+	}
+
+	require.NoError(t, Save(db, &user))
+	require.NoError(t, SoftDelete(db, &user, "DeletedAt"))
+
+	m := map[string]interface{}{"username": "thoas"}
+
+	query := `
+	SELECT count(*)
+	FROM %s
+	WHERE username = :username
+	AND deleted_at IS NULL
+	`
+
+	stmt, err := db.PrepareNamed(fmt.Sprintf(query, user.TableName()))
+	require.NoError(t, err)
+
+	var count int
+
+	err = stmt.Get(&count, m)
+	require.NoError(t, err)
+
+	is.Equal(0, count)
 }
