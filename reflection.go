c5ab25b9d1614c916419cdd64c1e52a2d62ca2fb
Gilles Fabio <gilles@gillesfabio.com>
2016-11-25 20:55:53 +0100

    Improve reflection utils.

--- reflection.go
+++ reflection.go
@@ -66,6 +66,41 @@ func getReflectedValue(entity interface{}) reflect.Value {
 }
 
 // isZeroValue returns true if the given interface is a zero value.
-func isZeroValue(entity interface{}) bool {
-	return !reflect.Indirect(reflect.ValueOf(entity)).IsValid()
+func isZeroValue(itf interface{}) bool {
+	v := reflect.Indirect(reflect.ValueOf(itf))
+	return v.Interface() == reflect.Zero(v.Type()).Interface()
+}
+
+// getStructFields returns struct fields of value.
+func getStructFields(v reflect.Value) []reflect.StructField {
+	if v.Kind() != reflect.Struct {
+		return nil
+	}
+
+	fields := []reflect.StructField{}
+
+	for i := 0; i < v.NumField(); i++ {
+		fields = append(fields, v.Type().Field(i))
+	}
+
+	return fields
+}
+
+// interfaceToModel cast an interface to a Model.
+func interfaceToModel(itf interface{}) Model {
+	v := reflect.Indirect(reflect.ValueOf(itf))
+
+	if v.IsValid() && v.Kind() != reflect.Slice {
+		return reflect.ValueOf(itf).Interface().(Model)
+	}
+
+	var typ reflect.Type
+
+	if reflect.Indirect(v).Kind() == reflect.Slice {
+		typ = v.Type().Elem()
+	} else {
+		typ = reflect.Indirect(v).Type()
+	}
+
+	return reflect.New(typ).Interface().(Model)
 }
