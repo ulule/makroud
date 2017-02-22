d6a6c78ec5753b1f82e2bfbe812a5789c9b95553
Gilles Fabio <gilles@gillesfabio.com>
2017-02-22 18:25:34 +0100

    Add new association model.

---
+++ associations.go
@@ -0,0 +1,77 @@
+package sqlxx
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/serenize/snaker"
+)
+
+// AssociationType is an association type.
+type AssociationType uint8
+
+// Association types
+const (
+	AssociationTypeUndefined = AssociationType(iota)
+	AssociationTypeOne
+	AssociationTypeMany
+	AssociationTypeManyToMany
+)
+
+// Association is a field association.
+type Association struct {
+	Type              AssociationType
+	RelatedModelName  string
+	RelatedFieldName  string
+	RelatedColumnName string
+}
+
+// NewAssociation returns a new Association instance for the given struct field.
+// And a boolean either the given field is an association or not.
+func NewAssociation(f reflect.StructField) (*Association, bool, error) {
+	var (
+		t               = f.Type
+		associationType = AssociationTypeUndefined
+	)
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Struct || t.Kind() == reflect.Slice {
+		associationType = AssociationTypeOne
+
+		if t.Kind() == reflect.Slice {
+			associationType = AssociationTypeMany
+			t = t.Elem()
+			if t.Kind() == reflect.Ptr {
+				t = t.Elem()
+			}
+		}
+
+		if _, ok := reflect.New(t).Interface().(Model); !ok {
+			associationType = AssociationTypeUndefined
+		}
+	}
+
+	if associationType == AssociationTypeUndefined {
+		return nil, false, nil
+	}
+
+	var (
+		relatedModel     = GetModelFromType(t)
+		relatedModelName = GetModelName(relatedModel)
+	)
+
+	relatedSchema, err := GetSchema(relatedModel)
+	if err != nil {
+		return nil, true, err
+	}
+
+	return &Association{
+		Type:              associationType,
+		RelatedModelName:  relatedModelName,
+		RelatedFieldName:  relatedSchema.PrimaryKeyField.Name,
+		RelatedColumnName: fmt.Sprintf("%s_%s", snaker.CamelToSnake(relatedModelName), relatedSchema.PrimaryKeyField.ColumnName),
+	}, true, nil
+}
