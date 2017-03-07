c992882385d53fd82bb669e6b00684c5ee6515c9
Gilles Fabio <gilles@gillesfabio.com>
2017-03-07 16:44:06 +0100

    Fix IntToInt64 function.

--- utils.go
+++ utils.go
@@ -59,6 +59,10 @@ func IntToInt64(value interface{}) (int64, error) {
 		v         = reflect.Indirect(reflect.ValueOf(value))
 	)
 
+	if !v.IsValid() {
+		return 0, fmt.Errorf("invalid value: %v", value)
+	}
+
 	if !v.Type().ConvertibleTo(int64Type) {
 		return 0, fmt.Errorf("unable to convert %v to int64", v.Type())
 	}
