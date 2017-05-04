a2c35006925a04eb0ca15042394857a886a6a8f5
Gilles Fabio <gilles@gillesfabio.com>
2017-05-04 18:25:17 +0200

    Test queries.

--- preload_test.go
+++ preload_test.go
@@ -16,8 +16,9 @@ func TestPreload_Unaddressable(t *testing.T) {
 
 	article := Article{}
 
-	_, err := sqlxx.Preload(db, article, "Author")
+	queries, err := sqlxx.Preload(db, article, "Author")
 	assert.Error(t, err)
+	assert.Nil(t, queries)
 }
 
 func TestPreload_UnknownRelation(t *testing.T) {
@@ -26,8 +27,9 @@ func TestPreload_UnknownRelation(t *testing.T) {
 
 	article := fixtures.Articles[0]
 
-	_, err := sqlxx.Preload(db, &article, "Foo")
+	queries, err := sqlxx.Preload(db, &article, "Foo")
 	assert.Error(t, err)
+	assert.Nil(t, queries)
 	assert.Zero(t, article.Author)
 }
 
@@ -37,49 +39,58 @@ func TestPreload_NullPrimaryKey(t *testing.T) {
 
 	category := createCategory(t, db, "cat1", nil)
 
-	_, err := sqlxx.Preload(db, &category, "User")
+	queries, err := sqlxx.Preload(db, &category, "User")
 	assert.NoError(t, err)
+	assert.Nil(t, queries)
 	assert.Zero(t, category.User)
 
 	category = createCategory(t, db, "cat1", &fixtures.User.ID)
 
-	_, err = sqlxx.Preload(db, &category, "User")
+	queries, err = sqlxx.Preload(db, &category, "User")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "FROM users WHERE users.id = ? LIMIT 1")
+	assert.Len(t, queries[0].Args, 1)
+	assert.Equal(t, category.UserID.Int64, queries[0].Args[0])
 	assert.NotZero(t, category.UserID)
 	assert.NotZero(t, category.User.ID)
 }
-
-// ----------------------------------------------------------------------------
-// Preloads: OneToOne
-// ----------------------------------------------------------------------------
 
 func TestPreload_OneToOne_Level1(t *testing.T) {
 	db, _, shutdown := dbConnection(t)
 	defer shutdown()
 
 	batman := createUser(t, db, "batman")
+	createUser(t, db, "spiderman")
 	article := createArticle(t, db, &batman)
 
-	//
-	// Instance
-	//
-
 	// Value
-	_, err := sqlxx.Preload(db, &article, "Author")
+	queries, err := sqlxx.Preload(db, &article, "Author")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "FROM users WHERE users.id = ? LIMIT 1")
+	assert.Len(t, queries[0].Args, 1)
+	assert.EqualValues(t, article.AuthorID, queries[0].Args[0])
 	assert.NotZero(t, article.Author)
 	assert.Equal(t, batman.ID, article.AuthorID)
 	assert.Equal(t, batman.Username, article.Author.Username)
 
 	// Pointer
-	_, err = sqlxx.Preload(db, &article, "Reviewer")
+	queries, err = sqlxx.Preload(db, &article, "Reviewer")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "FROM users WHERE users.id = ? LIMIT 1")
+	assert.Len(t, queries[0].Args, 1)
+	assert.EqualValues(t, article.ReviewerID, queries[0].Args[0])
 	assert.NotZero(t, article.Reviewer)
 	assert.Equal(t, batman.ID, article.ReviewerID)
 	assert.Equal(t, batman.Username, article.Reviewer.Username)
 }
 
-func TestPreload_ManyToOne_Level1_Same(t *testing.T) {
+func TestPreload_ManyToOne_Level1(t *testing.T) {
 	db, _, shutdown := dbConnection(t)
 	defer shutdown()
 
@@ -91,48 +102,61 @@ func TestPreload_ManyToOne_Level1_Same(t *testing.T) {
 	}
 
 	// Value
-	_, err := sqlxx.Preload(db, &articles, "Author")
+	queries, err := sqlxx.Preload(db, &articles, "Author")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "FROM users WHERE users.id = ?")
+	assert.Len(t, queries[0].Args, 1)
+
 	for _, a := range articles {
 		assert.Equal(t, batman.ID, a.AuthorID)
 		assert.Equal(t, batman.Username, a.Author.Username)
+		assert.EqualValues(t, a.AuthorID, queries[0].Args[0])
 	}
 
 	// Pointer
-	_, err = sqlxx.Preload(db, &articles, "Reviewer")
+	queries, err = sqlxx.Preload(db, &articles, "Reviewer")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "FROM users WHERE users.id = ?")
+	assert.Len(t, queries[0].Args, 1)
+
 	for _, a := range articles {
 		assert.Equal(t, batman.ID, a.ReviewerID)
 		assert.Equal(t, batman.Username, a.Reviewer.Username)
+		assert.EqualValues(t, a.ReviewerID, queries[0].Args[0])
 	}
 }
 
-func TestPreload_ManyToOne_Level1_Different_Pointer_Null(t *testing.T) {
+func TestPreload_Slice_ManyToOne_Level1_Different_Pointer_Null(t *testing.T) {
 	db, _, shutdown := dbConnection(t)
 	defer shutdown()
 
 	batman := createUser(t, db, "batman")
 	robin := createUser(t, db, "robin")
 	catwoman := createUser(t, db, "catwoman")
-
-	users := []User{
-		batman,
-		robin,
-		catwoman,
-	}
+	users := []User{batman, robin, catwoman}
 
 	// user_id => media_id
 	avatars := map[int]int{}
-
 	for _, user := range users {
 		avatars[user.ID] = int(user.AvatarID.Int64)
 	}
 
-	_, err := sqlxx.Preload(db, &users, "Avatar")
+	queries, err := sqlxx.Preload(db, &users, "Avatar")
 	assert.NoError(t, err)
-	for i, _ := range users {
-		assert.NotNil(t, users[i].Avatar)
-		assert.Equal(t, users[i].Avatar.ID, avatars[users[i].ID])
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "WHERE media.id IN (?, ?, ?)")
+	assert.Len(t, queries[0].Args, 3)
+
+	for _, user := range users {
+		avatar := user.Avatar
+		assert.NotNil(t, avatar)
+		assert.Equal(t, avatar.ID, avatars[user.ID])
+		assert.Contains(t, queries[0].Args, int64(avatar.ID))
 	}
 }
 
@@ -143,26 +167,29 @@ func TestPreload_ManyToOne_Level1_Different(t *testing.T) {
 	batman := createUser(t, db, "batman")
 	robin := createUser(t, db, "robin")
 	catwoman := createUser(t, db, "catwoman")
+
 	article1 := createArticle(t, db, &batman)
 	article2 := createArticle(t, db, &robin)
 	article3 := createArticle(t, db, &catwoman)
+	articles := []Article{article1, article2, article3}
 
-	articles := []Article{
-		article1,
-		article2,
-		article3,
-	}
-
-	_, err := sqlxx.Preload(db, &articles, "Author", "Reviewer")
+	queries, err := sqlxx.Preload(db, &articles, "Author", "Reviewer")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 2)
+	assert.Contains(t, queries[0].Query, "WHERE users.id IN")
+	assert.Len(t, queries[0].Args, 3)
+
 	assert.Equal(t, articles[0].AuthorID, batman.ID)
 	assert.NotZero(t, articles[0].Author)
 	assert.Equal(t, articles[0].ReviewerID, batman.ID)
 	assert.NotZero(t, articles[0].Reviewer)
+
 	assert.Equal(t, articles[1].AuthorID, robin.ID)
 	assert.NotZero(t, articles[1].Author)
 	assert.Equal(t, articles[1].ReviewerID, robin.ID)
 	assert.NotZero(t, articles[1].Reviewer)
+
 	assert.Equal(t, articles[2].AuthorID, catwoman.ID)
 	assert.NotZero(t, articles[2].Author)
 	assert.Equal(t, articles[2].ReviewerID, catwoman.ID)
@@ -182,10 +209,22 @@ func TestPreload_OneToOne_Level2(t *testing.T) {
 	defer shutdown()
 
 	user := createUser(t, db, "spiderman")
-
 	article := createArticle(t, db, &user)
-	_, err := sqlxx.Preload(db, &article, "Author", "Author.APIKey")
+
+	queries, err := sqlxx.Preload(db, &article, "Author", "Author.APIKey")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+
+	authorQuery, ok := queries.ByTable("users")
+	assert.True(t, ok)
+	assert.Contains(t, authorQuery.Query, "WHERE users.id = ? LIMIT 1")
+	assert.Len(t, authorQuery.Args, 1)
+
+	apikeyQuery, ok := queries.ByTable("api_keys")
+	assert.True(t, ok)
+	assert.Contains(t, apikeyQuery.Query, "WHERE api_keys.id = ? LIMIT 1")
+	assert.Len(t, apikeyQuery.Args, 1)
+
 	assert.NotZero(t, article.Author)
 	assert.NotZero(t, article.Author.APIKey)
 	assert.Equal(t, user.ID, article.AuthorID)
@@ -194,16 +233,21 @@ func TestPreload_OneToOne_Level2(t *testing.T) {
 	assert.Equal(t, "spiderman-apikey", article.Author.APIKey.Key)
 }
 
-func TestPreload_OneToOne_Level2_MultipleEither(t *testing.T) {
+func TestPreload_OneToOne_Level2_ValueAndPointer(t *testing.T) {
 	db, _, shutdown := dbConnection(t)
 	defer shutdown()
 
 	user := createUser(t, db, "spiderman")
 	assert.NotEmpty(t, user)
 
-	_, err := sqlxx.Preload(db, &user, "Avatar")
+	queries, err := sqlxx.Preload(db, &user, "Avatar")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "FROM media WHERE media.id = ? LIMIT 1")
+	assert.Len(t, queries[0].Args, 1)
 	assert.NotNil(t, user.Avatar)
+	assert.EqualValues(t, user.AvatarID.Int64, queries[0].Args[0])
 
 	article := createArticle(t, db, &user)
 	assert.NotEmpty(t, article)
@@ -213,10 +257,27 @@ func TestPreload_OneToOne_Level2_MultipleEither(t *testing.T) {
 
 	comments := []Comment{comment}
 
-	// Preload
-	_, err = sqlxx.Preload(db, &comments, "User", "User.Avatar")
+	queries, err = sqlxx.Preload(db, &comments, "User", "User.Avatar")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 2)
+
 	comment = comments[0]
+
+	userQuery, ok := queries.ByTable("users")
+	assert.True(t, ok)
+
+	avatarQuery, ok := queries.ByTable("media")
+	assert.True(t, ok)
+
+	assert.Contains(t, userQuery.Query, "FROM users WHERE users.id = ?")
+	assert.Len(t, userQuery.Args, 1)
+	assert.EqualValues(t, comment.User.ID, userQuery.Args[0])
+
+	assert.Contains(t, avatarQuery.Query, "WHERE media.id IN (?)")
+	assert.Len(t, avatarQuery.Args, 1)
+	assert.NotNil(t, user.Avatar)
+	assert.EqualValues(t, comment.User.Avatar.ID, avatarQuery.Args[0])
 
 	// Level 1 with Value
 	assert.NotZero(t, comment.User)
@@ -230,47 +291,58 @@ func TestPreload_OneToOne_Level2_MultipleEither(t *testing.T) {
 	}
 }
 
-func TestPreload_ManyToOne_Level2_Multiple(t *testing.T) {
+func TestPreload_ManyToOne_Level2_ValueAndPointer(t *testing.T) {
 	db, _, shutdown := dbConnection(t)
 	defer shutdown()
 
 	user := createUser(t, db, "spiderman")
 	article := createArticle(t, db, &user)
-
 	deadpool := createUser(t, db, "deadpool")
 	article2 := createArticle(t, db, &deadpool)
-
 	articles := []Article{article, article2}
 
-	_, err := sqlxx.Preload(db, &articles, "Author", "Author.APIKey")
+	queries, err := sqlxx.Preload(db, &articles, "Author", "Author.APIKey")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 2)
+
+	authorQuery, ok := queries.ByTable("users")
+	assert.True(t, ok)
+	assert.Contains(t, authorQuery.Query, "WHERE users.id IN (?, ?)")
+
+	apikeyQuery, ok := queries.ByTable("api_keys")
+	assert.True(t, ok)
+	assert.Contains(t, apikeyQuery.Query, "WHERE api_keys.id IN (?, ?)")
 
 	assert.Equal(t, user.ID, articles[0].Author.ID)
 	assert.Equal(t, user.ID, articles[0].AuthorID)
+
 	assert.Equal(t, user.Username, articles[0].Author.Username)
 	assert.NotZero(t, articles[0].Author.APIKeyID)
-
 	assert.Equal(t, "spiderman-apikey", articles[0].Author.APIKey.Key)
 
 	assert.Equal(t, deadpool.ID, articles[1].Author.ID)
 	assert.Equal(t, deadpool.ID, articles[1].AuthorID)
 	assert.Equal(t, deadpool.Username, articles[1].Author.Username)
+
 	assert.NotZero(t, articles[1].Author.APIKeyID)
 	assert.Equal(t, "deadpool-apikey", articles[1].Author.APIKey.Key)
 }
 
-// ----------------------------------------------------------------------------
-// Preloads: ToMany
-// ----------------------------------------------------------------------------
-
-func TestPreload_OneToMany_Level1_Simple(t *testing.T) {
+func TestPreload_OneToMany_Level1(t *testing.T) {
 	db, _, shutdown := dbConnection(t)
 	defer shutdown()
 
 	user := createUser(t, db, "wonderwoman")
 
-	_, err := sqlxx.Preload(db, &user, "Avatars")
+	queries, err := sqlxx.Preload(db, &user, "Avatars")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "WHERE avatars.user_id = ?")
+	assert.Len(t, queries[0].Args, 1)
+	assert.EqualValues(t, user.ID, queries[0].Args[0])
+
 	assert.Len(t, user.Avatars, 5)
 	for i, a := range user.Avatars {
 		assert.NotZero(t, a.ID)
@@ -292,11 +364,17 @@ func TestPreload_ManyToMany_Level1(t *testing.T) {
 		assert.Zero(t, user.Avatars)
 	}
 
-	_, err := sqlxx.Preload(db, &users, "Avatars")
-
+	queries, err := sqlxx.Preload(db, &users, "Avatars")
 	assert.NoError(t, err)
+	assert.NotNil(t, queries)
+	assert.Len(t, queries, 1)
+	assert.Contains(t, queries[0].Query, "avatars.user_id IN (?, ?, ?, ?, ?)")
+	assert.Len(t, queries[0].Args, 5)
+
 	for _, user := range users {
 		assert.NotZero(t, user.Avatars)
+		assert.Contains(t, queries[0].Args, int64(user.ID))
+
 		for _, avatar := range user.Avatars {
 			assert.NotZero(t, avatar.ID)
 			assert.Equal(t, user.ID, avatar.UserID)
