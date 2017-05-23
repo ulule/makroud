0887820d737401c399589cbefe1829ec0418cd8f
Thomas LE ROUX <thomas@leroux.io>
2017-05-23 10:21:07 +0200

    feat(helpers): add exec and find with InParams

---
+++ helpers_test.go
@@ -0,0 +1,80 @@
+package sqlxx_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ulule/sqlxx"
+)
+
+func TestExec_InParams(t *testing.T) {
+	env := setup(t)
+	defer env.teardown()
+
+	is := require.New(t)
+
+	env.createUser("batman")
+	env.createUser("robin")
+	env.createUser("catwoman")
+
+	query := "UPDATE users SET is_active = false WHERE username IN (?);"
+	params := []string{"batman", "robin", "catwoman"}
+	queries, err := sqlxx.ExecInParamsWithQueries(env.driver, query, params)
+	is.NoError(err)
+	is.Len(queries, 1)
+	is.Equal("UPDATE users SET is_active = false WHERE username IN (?, ?, ?);", queries[0].Query)
+	is.Equal([]interface{}{"batman", "robin", "catwoman"}, queries[0].Args)
+
+	query = "SELECT COUNT(*) FROM users WHERE is_active = :is_active"
+	stmt, err := env.driver.PrepareNamed(query)
+	is.NoError(err)
+	is.NotNil(stmt)
+
+	count := 0
+	err = stmt.Get(&count, map[string]interface{}{
+		"is_active": false,
+	})
+	is.NoError(err)
+	is.Equal(3, count)
+}
+
+func TestFind_InParams(t *testing.T) {
+	env := setup(t)
+	defer env.teardown()
+
+	is := require.New(t)
+
+	env.createUser("batman")
+	env.createUser("robin")
+	env.createUser("catwoman")
+
+	query := "SELECT * FROM users WHERE is_active = true AND username IN (?);"
+	users := &[]User{}
+	params := []string{"batman", "robin", "catwoman"}
+	queries, err := sqlxx.FindInParamsWithQueries(env.driver, users, query, params)
+	is.NoError(err)
+	is.Len(queries, 1)
+	is.Equal("SELECT * FROM users WHERE is_active = true AND username IN (?, ?, ?);", queries[0].Query)
+	is.Equal([]interface{}{"batman", "robin", "catwoman"}, queries[0].Args)
+
+	is.Len(*users, 3)
+	hasBatman := false
+	hasRobin := false
+	hasCatwoman := false
+	for _, user := range *users {
+		switch user.Username {
+		case "batman":
+			hasBatman = true
+		case "robin":
+			hasRobin = true
+		case "catwoman":
+			hasCatwoman = true
+		default:
+			is.FailNow("unexpected username", user.Username)
+		}
+	}
+	is.True(hasBatman)
+	is.True(hasRobin)
+	is.True(hasCatwoman)
+}
