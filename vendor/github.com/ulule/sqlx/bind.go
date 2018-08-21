abe5b8104b9dfa9c52be6f63f1b4d33a89e33183
Thomas LE ROUX <thomas@leroux.io>
2018-08-21 10:30:22 +0200

    chore: Update sqlx fork

--- vendor/github.com/ulule/sqlx/bind.go
+++ vendor/github.com/ulule/sqlx/bind.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/jmoiron/sqlx/reflectx"
+	"github.com/ulule/sqlx/reflectx"
 )
 
 // Bindvar types supported by Rebind, BindMap and BindStruct.
--- vendor/github.com/ulule/sqlx/sqlx.go
+++ vendor/github.com/ulule/sqlx/sqlx.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/jmoiron/sqlx/reflectx"
+	"github.com/ulule/sqlx/reflectx"
 )
 
 // Although the NameMapper is convenient, in practice it should not
