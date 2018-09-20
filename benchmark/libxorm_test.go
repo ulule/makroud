2cd7e4ae34a45862b28fa652160f9ecf8252a9a2
Thomas LE ROUX <thomas@leroux.io>
2018-09-20 11:42:46 +0200

    fix: xorm model

--- benchmark/libxorm_test.go
+++ benchmark/libxorm_test.go
@@ -11,43 +11,39 @@ type PilotXorm struct {
 }
 
 type JetXorm struct {
-	Id int `xorm:"pk"`
-
-	PilotId int `xorm:"not null"`
-
-	AirportId int `xorm:"not null"`
-
+	ID         int    `xorm:"pk"`
+	PilotID    int    `xorm:"not null"`
+	AirportID  int    `xorm:"not null"`
 	Name       string `xorm:"not null"`
 	Color      sql.NullString
-	Uuid       string `xorm:"not null"`
+	UUID       string `xorm:"not null"`
 	Identifier string `xorm:"not null"`
 	Cargo      []byte `xorm:"not null"`
 	Manifest   []byte `xorm:"not null"`
 }
 
 type AirportXorm struct {
-	Id   int `xorm:"pk"`
+	ID   int `xorm:"pk"`
 	Size sql.NullInt64
 }
 
 type LicenseXorm struct {
-	Id int `xorm:"pk"`
-
+	ID      int `xorm:"pk"`
 	Pilot   PilotXorm
 	PilotId int
 }
 
 type HangarXorm struct {
-	Id   int    `xorm:"pk"`
+	ID   int    `xorm:"pk"`
 	Name string `xorm:"not null"`
 }
 
 type LanguageXorm struct {
-	Id       int    `xorm:"pk"`
+	ID       int    `xorm:"pk"`
 	Language string `xorm:"index not null"`
 }
 
 type PilotLanguageXorm struct {
-	PilotId    int `xorm:"pk"`
+	PilotID    int `xorm:"pk"`
 	LanguageId int `xorm:"pk"`
 }
