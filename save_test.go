d356c821d84400b59dce9da09c99d8ca879026db
LE ROUX Thomas <thomas@leroux.io>
2019-07-23 02:14:56 +0200

    chore: remove t.Skip()

--- save_test.go
+++ save_test.go
@@ -57,7 +57,6 @@ func TestSave_Owl(t *testing.T) {
 }
 
 func TestSave_Meow(t *testing.T) {
-	t.Skip()
 	Setup(t)(func(driver makroud.Driver) {
 		ctx := context.Background()
 		is := require.New(t)
